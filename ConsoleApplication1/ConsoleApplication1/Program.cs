6474f653d8823045771a33dc30ae2898cab5c51c
Hanan Hendy <Hanan.hendy@gmail.com>
2017-07-22 12:25:02 +0200

    Adding x & y

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,8 +8,12 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        public static int x { get; set; }
+        public static int y { get; set; }
         static void Main(string[] args)
         {
+            x = 10;
+            y = 10;
         }
     }
 }
